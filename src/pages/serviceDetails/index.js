f88c091ef5cf4641fd0fca402d5b342b6416897c
samuel <samuel.liotta@gmail.com>
2021-04-02 19:15:53 +0200

    fetchServiceById refactored: now it gets data frome firestore only if I select a new service. If I select the previous one, there is no new dowload

--- src/pages/serviceDetails/index.js
+++ src/pages/serviceDetails/index.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { serviceOperations } from "../../redux/service";
-import { useDispatch } from "react-redux";
+import { serviceOperations, serviceSelectors } from "../../redux/service";
+import { useDispatch, useSelector } from "react-redux";
 import ServiceDetailsComponent from "./serviceDetails.component";
 
 const ServiceDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const currentService = useSelector(serviceSelectors.service);
 
   useEffect(() => {
-    dispatch(serviceOperations.fetchServiceById(id));
+    dispatch(serviceOperations.fetchServiceById(id, currentService.id));
   }, []);
 
   return <ServiceDetailsComponent />;
--- src/redux/service/operations.js
+++ src/redux/service/operations.js
@@ -1,8 +1,13 @@
 import { serviceActions } from "./index";
 import fireApi from "../../api/firebase";
 
-export const fetchServiceById = (id) => async (dispatch) => {
+export const fetchServiceById = (id, idInStore) => async (dispatch) => {
+  if (id === idInStore) {
+    return Promise.resolve();
+  }
   dispatch(serviceActions.fetchingStarted());
   const service = await fireApi.getServiceById(id);
+  //maybe refactore: ex. if service = 'error', serviceActions.error = true
+  //invece che ritorare oggetto item = {error: true}
   dispatch(serviceActions.fetchServiceSuccess(service));
 };
